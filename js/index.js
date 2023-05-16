1bb34f8161c46b39c62cfc82f2b4e519feece762
tamryn22 <125568338+tamryn22@users.noreply.github.com>
2023-05-16 13:37:18 +0200

    forms and function
    adding functions with if to forms

---
+++ js/index.js
@@ -0,0 +1,11 @@
+calculate = () => {
+    let valueOne= +document.getElementById("valueOne").value
+    let valueTwo = +document.getElementById("valueTwo").value 
+    let operator =document.getElementById("operator").value
+    console.log(valueOne, valueTwo, operator)
+    // if(age >= 18){
+    //     alert("The Student , "+ first + " " + last + " has been added to " + subject)
+    // }else{
+    //     alert("The student is not old enough to enroll")
+    // }
+}
